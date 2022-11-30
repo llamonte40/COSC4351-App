5758a59a8936ec749da6442e8f7b13e2b6d71b29
sevbansonmez <101483923+sevbansonmez@users.noreply.github.com>
2022-11-30 18:05:25 +0300

    Sevban development (#2)
    * Add db connection, schema and crud operations
    
    * Add crud for the reservation entity

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,92 @@
-from typing import Union
+from unicodedata import name
+from fastapi import FastAPI, HTTPException
+from pymongo import MongoClient
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 
-from fastapi import FastAPI
+class Reservation(BaseModel):
+    name : str
+    time: int
+    table_number: int
+    
+client = MongoClient('mongodb+srv://doadmin:5zRc20W74P6rgS39@db-mongodb-sfo3-80898-09a08f1a.mongo.ondigitalocean.com/admin?tls=true&authSource=admin')
+
+db = client["restaurant"]
+collection = db["reservation"]
 
 app = FastAPI()
 
+def check_table_availability(time: int, table: int) -> bool:
+    result = collection.find({"time": time, "table_number": table})
+    list_cursor = list(result)
+    return not len(list_cursor) > 0
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/reservation/by-name/{name}")
+def get_reservation_by_name(name:str):
+    query={"name": name }
+    query_result=collection.find(query, {"_id": 0})
+    result = []
+    for n in query_result:
+        result.append(n)
+    return {
+        "result": result
+    }
+
+@app.get("/reservation/by-table/{table}")
+def get_reservation_by_table(table: int):
+    query={"table_number":table}
+    query_result=collection.find(query, {"_id": 0})
+    result = []
+    for n in query_result:
+        result.append(n)
+    return {
+        "result": result
+    }
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/reservation")
+def reserve(reservation : Reservation):
+
+    if not check_table_availability(reservation.time, reservation.table_number):
+        # Incase that the reservation is found based on the condition above
+        raise HTTPException(status_code=400, detail={
+            "message": "That table isn't available at that time"
+        })
+    # Insert new data
+    insert_result = collection.insert_one({
+        "name": reservation.name,
+        "time": reservation.time,
+        "table_number": reservation.table_number
+    })
+    # Return response
+    return {
+        "message": "success",
+        "id": str(insert_result.inserted_id)
+    }
+
+@app.put("/reservation/update/")
+def update_reservation(reservation: Reservation):
+    query_find = {
+        "name": reservation.name
+        }
+    check = collection.find(query_find)
+    list_check = list(check)
+    if len(list_check)==0:
+        raise HTTPException(400,f"Couldn't find name:{reservation.name}")
+
+    if not check_table_availability(reservation.time,reservation.table_number):
+        raise HTTPException(400,f"Table not available")
+    collection.update_many({"name":reservation.name},{"$set":{"table_number":reservation.table_number,"time":reservation.time}}) 
+    return {
+        "message": "success"
+    }
+    
+        
+@app.delete("/reservation/delete/{name}/{table_number}")
+def cancel_reservation(name: str, table_number : int):
+    query = {
+        "name":name , 
+        "table_number":table_number
+         }
+    collection.delete_one(query)
+    return {}
